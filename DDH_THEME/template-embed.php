9f7c2fdb893f138f4f9af2d20d1bc691ea98853d
Justin Hammack <info@justinhammack.com>
2012-03-11 12:29:24 -0700

    added template for forums

---
+++ DDH_THEME/template-embed.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Vanilla Forum Template
+ *
+ * A custom page template for a Vanilla Forum.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ */
+
+get_header(); ?>
+
+<script type="text/javascript" src="http://ddh.bigfatnoob.com/plugins/embedvanilla/remote.js"></script>
+
+<?php get_footer();
